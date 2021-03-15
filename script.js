92f178678db0779a363f7e9490a1dac1058efa04
scadler <scadler@s.sfusd.edu>
2021-03-15 01:26:40 +0000

    idk what I did but Im pretty sure I did something

--- script.js
+++ script.js
@@ -5,34 +5,39 @@ var dots = {
     xy : [[],[]],
     v : [[],[]],
     step : [],
-    color : [],
+    color : [[],[]],
 }
-/*dots.xy[0 or 1][i] = [x,y], dots.v[0 or 1][i] = [vx,vy],
+/*dots.xy[0 or 1][i] = [x,y], dots.v[0 or 1][i] = [vx,vy], 
+dots.color[0 or 1] = [color,opacity]
 dots.step is num of interations since creation
 */
 function spawnDots(xCoor,yCoor){
-    var i = dots.xy[0].length
-    while(i < 20){
+    var i =  dots.xy[0].length
+    while(i < 25){
         dots.xy[0][i] = xCoor;
         dots.xy[1][i] = yCoor;
+        var velocity = 8+Math.random()
         var sign = (Math.random() < 0.5) ? -1 : 1;
-        dots.v[0][i] = (Math.random()*20)-10;
-        dots.v[1][i] = sign*Math.sqrt( 100 - (dots.v[0][i]*dots.v[0][i]))
-        dots.step[i] = 50+(Math.random()*100)
-        // var red   = Math.sin((Math.PI*(90+Math.random()*10))+2+1)*127+128
-        // var green  = Math.sin((Math.PI*(90+Math.random()*10))+1)*127+128
-        // var blue   = Math.sin((Math.PI*(90+Math.random()*10))+4+1)*127+128
-        dots.color[i] = "hsl(" + Math.random() * 360 + ", 100%, 75%, " +1+")";
-        //  "rgba("+red+", "+green+", "+blue+", "+1+")"
-        i++
+        dots.v[0][i] = (Math.random()*20)-velocity;
+        dots.v[1][i] = sign*Math.sqrt( (velocity*velocity) - (dots.v[0][i]*dots.v[0][i]))
+        dots.step[i] = 80+(Math.random()*100)
+        dots.color[0][i] = Math.random() * 360
+        dots.color[1][i] = dots.step[i]
+        i++   
     }
 }
-function drawDots(x, y, color){
-    context.fillStyle = color;
+// function spawnDots(xCoor,yCoor, i){
+//     if(i < 25){
+    
+//     setTimeout(function(){ spawnDots(xCoor,yCoor, i)}, Math.random()*8);
+//     }
+// }
+function drawDots(x, y, color, opacity){
+    context.fillStyle = "hsla(" + color+ ", 100%, 75%, " +opacity+")";
     context.beginPath();
-    context.arc(x, y, 2, 0, Math.PI*2, false);
+    context.arc(x, y, 4, 0, Math.PI*2, false);
     context.lineWidth = 1.5;
-    context.strokeStyle = color;
+    context.strokeStyle = "hsla(" + color+ ", 100%, 75%, " +opacity+")";
     context.closePath();
     context.fill();
     context.stroke();
@@ -48,6 +53,8 @@ function render(){
         while(i<dots.xy[0].length){
             dots.xy[0][i] += dots.v[0][i]/10
             dots.xy[1][i] += dots.v[1][i]/10
+            dots.v[0][i] *= 0.998
+            dots.v[1][i] += 0.1
             dots.step[i] -= 1
             if(dots.step[i] <= 0){
                 dots.step.splice(i,1)
@@ -55,9 +62,11 @@ function render(){
                 dots.xy[1].splice(i,1)
                 dots.v[0].splice(i,1)
                 dots.v[1].splice(i,1)
-                dots.color.splice(i,1)
+                dots.color[0][i].splice(i,1)
+                dots.color[1][i].splice(i,1)
             }
-            drawDots(dots.xy[0][i], dots.xy[1][i], dots.color[i])
+            console.log(dots.step[i]/dots.color[1][i])
+            drawDots(dots.xy[0][i], dots.xy[1][i], dots.color[0][i], Math.pow(dots.step[i]/dots.color[1][i], 0.25))
             i++
         }
     }
